fcae8d0132f77229547e1eff7e8ab4f56e67c394
VladimirPalanzeev <palanzey@gmail.com>
2022-02-23 15:16:52 +0500

    [upd] Загружаю картинки устанавливаю фон

--- 01.py
+++ 01.py
@@ -1,15 +1,26 @@
 from tkinter import *
 
+# Размеры окна
+WIDTH = 640
+HEIGHT = 480
+
 root = Tk()
-root["bg"] = "#000000"
-root.geometry("480x360")
+# root["bg"] = "#000000"
+
+root.geometry(f"{WIDTH}x{HEIGHT}")
 
 # Создаём виджет Canvas
-cnv = Canvas(root, width=240, height=180, bg="#AAAAAA")
+cnv = Canvas(root, width=WIDTH, height=HEIGHT)
 # Убираем рамку
-cnv.config(highlightthickness=0)
-
+cnv.config(highlightthickness=1)
 # Устанавливаем в область окна
-cnv.place(x=50, y=50)
+cnv.place(x=0, y=0)
+# Устанавливаем фокус внимания
+cnv.focus_set()
+
+# Загружаем фоновую картинку
+back = PhotoImage(file="imageForCanvas/background.png")
+# И устанавливаем её на Canvas
+cnv.create_image(WIDTH // 2, HEIGHT // 2, image=back)
 
 root.mainloop()
