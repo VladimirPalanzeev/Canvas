48ad3fcf0a68cc5ac08960dbb563c6adb8f3b8a8
VladimirPalanzeev <palanzey@gmail.com>
2022-02-27 21:32:53 +0500

    [upd] Методы движения игрока и бота

--- 04.py
+++ 04.py
@@ -1,12 +1,52 @@
 from tkinter import *
 
 # Перемещение квадрата игрока
-def move(veknor):
-    pass
+def move(vector):
+    # vector - направление движения
+    if vector == UPKEY:
+        cnv.move(player, 0, -playerSpeed)
+    elif vector == DOWNKEY:
+        cnv.move(player, 0, playerSpeed)
+    elif vector == LEFTKEY:
+        cnv.move(player, -playerSpeed, 0)
+    elif vector == RIGHTKEY:
+        cnv.move(player, playerSpeed, 0)
+
 
 # Неумолимое движение вражеского круга
 def evilMove():
-    pass
+    global evilAfter, vectorX, vectorY
+
+    # Передвигаем вражеский круг
+    cnv.move(evil, vectorX, vectorY)
+
+    # Получаем координаты в отдельные переменные так легче читать и считать код
+    # cnv.coords возвращает список, в котором на нулевой позиции стоит координата x,
+    # на первой - координата y
+    x = cnv.coords(evil)[0]
+    y = cnv.coords(evil)[1]
+
+    # Проверяем: ударился ли красный круг щ границы окна
+    if x > WIDTH - 32 or x < 32:
+        vectorX = -vectorX
+    if y > HEIGHT - 32 or y < 32:
+        vectorY = -vectorY
+
+    # Получаем координаты игрока
+    xP = cnv.coords(player)[0]
+    yP = cnv.coords(player)[1]
+
+    # Считаем по теореме Пифагора. Если расстояние между координатами меньше диаметра круга,
+    # то засчитываем соприкосновение и останавливаем перемещение вражеского круга
+
+    distance = (abs(x - xP) ** 2 + abs(y - yP) ** 2) ** 0.5
+    if distance < 64:
+        # Если соприкосновение, то останавливаем evilAfter
+        root.after_cancel(evilAfter)
+    else:
+        # Если соприкосновения нет, то перемещаем вражеский круг дальше
+        evilAfter = root.after(30, evilMove)
+
 
 
 # *****************************************************
